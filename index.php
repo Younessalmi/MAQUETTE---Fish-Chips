96b1130fc8b008f0b084be136540ae38aa05fc3f
Younessalmi <younessalmi193@gmail.com>
2021-08-07 19:07:05 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fish Chips</title>
+
+    <style>
+    html{
+        background-image: url("./img/fond.png");
+    }
+
+    header{
+        background-color:#34b1b2;
+        height:250px;
+        width:100%;
+        margin:0px auto;
+        border-top:5px solid #2c9da2;
+        border-bottom:5px solid #2c9da2;
+        text-align:center;
+        margin-top:100px;
+    }
+
+    h1{
+      text-align:center;  
+      font-size:50px;
+
+    }
+
+    h2{
+      text-align:center;  
+      font-size:30px;
+      font-family:arial;
+    }
+
+    h3{
+        font-size:25px;
+        font-family:arial;
+    }
+  
+    </style>
+</head>
+<body>
+
+    <header>
+
+        <img src="./img/logo.png" style="margin-top:12px;">
+        <ul>
+            <a><li style="font-family:arial;display:inline-block;margin-right:15px;color:white;font-size:25px;">Location</a></li>
+            <a><li style="font-family:arial;display:inline-block;margin-right:15px;color:white;font-size:25px;">About us</a></li>
+            <a><li style="font-family:arial;display:inline-block;margin-right:15px;color:white;font-size:25px;">Contact</a></li>
+        </ul>
+ 
+    </header>
+
+
+<div style="margin-left:500px;margin-right:500px;margin-bottom:50px;">
+
+    <h2>Welcome to Fish & Chips !</h2>
+
+    <p style="text-align:center;font-family:arial;">Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate conmerciorum castrisque oppleta validis et castellis, 
+    quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.</p>
+    <p style="text-align:center;font-family:arial;">haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas. hanc
+     provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe.</p>
+    <p style="text-align:center;font-family:arial;">Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate conmerciorum castrisque oppleta validis et castellis, 
+    quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.</p>
+    <p style="text-align:center;font-family:arial;">haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.
+     hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe.</p>
+
+</div>
+
+
+
+<div style="background-color:#b6dedc;width:800px;height:810px;margin-left:300px;">
+    <h1><a style="color:#33b2b2;"> Today's</a> Special</h1> 
+    <div style="padding:30px; float:left;width:40%;">
+       <h4 style="font-family:arial;font-size:25px;text-align:center;">Le crusty fish</h4>
+        <p style="font-family:arial;font-size:17px;"><img src="./img/recipe1.jpg" alt="mini_logo" style="float:left;border-radius: 50%;margin-right:15px;">
+         Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate
+         conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil
+         veterum per oportunos saltus erexit et cautos.</br></br>
+
+         phaec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.
+          hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe.</br></br>
+
+          Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate conmerciorum castrisque oppleta validis et castellis.</p>
+
+            <div style="margin-left:170px;">
+                <button style="background-color:#34b2b2;border:solid 0px #56676f;color:white;padding: 10px 20px;">Read More</button>
+            </div>   
+    </div>
+
+    <div style="padding:30px;float:right;width:40%;">
+       <h4 style="font-family:arial;font-size:25px;text-align:center;">La multifish</h4>
+        <p style="font-family:arial;font-size:17px;"><img src="./img/recipe1.jpg" alt="mini_logo" style="float:left;border-radius: 50%;margin-right:15px;">
+         Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate
+         conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil
+         veterum per oportunos saltus erexit et cautos.</br></br>
+
+         phaec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.
+          hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe.</br></br>
+
+          Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.</p>
+
+            <div style="margin-left:170px;">
+                <button style="background-color:#34b2b2;border:solid 0px #56676f;color:white;padding: 10px 20px;">Read More</button>
+            </div>   
+    </div>     
+</div>
+
+<div style="background-color:#34b1b2;height:800px;width:291px;float:right;text-align:center;position:absolute;right:310px;top:772px;
+border-bottom:solid 5px #2b9aa0;border-top:solid 5px #2b9aa0;border-right:solid 5px #2b9aa0;border-left:solid 5px #2b9aa0;">
+    <h2>News<a style="color:white;">& events</a></h2> <img src="./img/vignette.png" alt="fish" style="position:absolute;top:-82px;right:-85px;">
+    
+    <p style="font-family:arial;font-size:18px;color:white;">
+    Huic Arabia est conserta, ex alio latere Nabataeis contigua opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium.<br><br>
+    </p>
+    <h3 style="color:#B6DFDD;font-size:18px;">18.01.15</h3>   
+    <p style="font-family:arial;font-size:18px;color:white;padding-left:3px;">
+    Huic Arabia est conserta, ex alio latere Nabataeis contigua opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium.<br><br>
+    </p>
+    <h3 style="color:#B6DFDD;font-size:18px;">18.01.15</h3>
+    <p style="font-family:arial;font-size:18px;color:white;">
+    Huic Arabia est conserta, ex alio latere Nabataeis contigua opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium.<br><br>
+    </p>
+    <h3 style="color:#B6DFDD;font-size:18px;">18.01.15</h3>
+    <p style="font-family:arial;font-size:18px;color:white;">
+    Huic Arabia est conserta, ex alio latere Nabataeis contigua opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium.<br><br>
+    </p>
+
+</p>
+</div>
+
+
+
+<br>
+<footer>
+<div>
+        <div style="text-align: center;">
+        <img style="width:70px;" src="./img/footer.png" alt="footer">
+        <div>
+<p style="text-align:center;font-size:12px;">
+ Cet exercice de <a href="#">3W Academy</a> est mis à disposition <a href="#">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution - 
+ Pas d'Utilisation Commerciale - Pas de Modification - International.</a><br>Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de 
+<a href="mailto:contact@3wa.fr"> contact@3wa.fr</a>. Les maquettes ont été réalisées par Justine Muller.</p>
+</div>
+</footer>
+</body>
+</html>
